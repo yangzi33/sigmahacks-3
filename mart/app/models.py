dec056db84287d7d23e871015e59fca7344a56ea
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2021-06-26 20:24:13 -0700

    followers model

--- mart/app/models.py
+++ mart/app/models.py
@@ -4,6 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table("followers", 
+        db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+        db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
+    )
 
 # db structure for a user
 class User(UserMixin, db.Model):
@@ -12,10 +16,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    followers = db.Table("followers", 
-        db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
-        db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
-    )
+
     followed = db.relationship(
         "User", secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -59,11 +60,11 @@ class User(UserMixin, db.Model):
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-    followed = Post.query.join(
-        followers, (followers.c.followed_id == Post.user_id)).filter(
-            followers.c.follower_id == self.id)
-    own = Post.query.filter_by(user_id=self.id)
-    return followed.union(own).order_by(Post.timestamp.desc())
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 # db structrue for a post
