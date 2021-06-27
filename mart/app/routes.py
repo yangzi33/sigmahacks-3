195a272aa00d80ba81745ec390256664605b1a3c
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2021-06-26 19:07:25 -0700

    User profile change

--- mart/app/routes.py
+++ mart/app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 
 # For user authentication/login/logout
 from flask_login import current_user, login_user, logout_user
@@ -100,3 +100,42 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+
+# Edit profile view
+@app.route("/edit_profile", methods=["POST", "GET"])        
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved.")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
